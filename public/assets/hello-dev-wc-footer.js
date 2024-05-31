534607c68a9e00196e5f3a4b0bf13881fbb4cc22
rohanharikr <rohanharikumar80@gmail.com>
2024-05-31 12:53:40 +0100

    update mail icon

--- public/assets/hello-dev-wc-footer.js
+++ public/assets/hello-dev-wc-footer.js
@@ -181,11 +181,7 @@ class Footer extends HTMLElement {
             <ul id="social-links">
               <li>
                 <a href="mailto:contact@hello.coop?subject=Hellō Inquiry" aria-label="Send an email">
-                  <svg id="mail-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                    <title>Email</title>
-                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                  </svg>
+                  <img src="https://cdn.hello.coop/images/mail-icon.svg" alt="Mail" width="16" height="16" />
                 </a>
               </li>
               <li>
