be4fdd73cc1d1d4a2f0c6b525796ea2d2c1713c1
rohanharikr <rohanharikumar80@gmail.com>
2024-12-06 10:52:57 +0000

    updates

---
+++ quickstart/src/lib/pages/Login.svelte
@@ -0,0 +1,38 @@
+<script>
+    // import { onMount } from "svelte"
+  
+    // export let login = () => {}
+    // export let loginAjax = false
+    // let darkMode = true
+  
+    // onMount(() => {
+    //   if (
+    //     window.matchMedia &&
+    //     window.matchMedia("(prefers-color-scheme: light)").matches
+    //   ) {
+    //     darkMode = false
+    //   }
+    //   window
+    //     .matchMedia("(prefers-color-scheme: dark)")
+    //     .addEventListener("change", (event) => {
+    //       if (event.matches) {
+    //         darkMode = true
+    //       } else {
+    //         darkMode = false
+    //       }
+    //     })
+    // })
+  </script>
+  
+  <h1 class="text-2xl font-semibold text-center">Quickstart App</h1>
+  <p class="text-center mt-4">
+    Quickstart accelerates getting up and running with Hellō. It will read an
+    existing client_id, or create one.
+  </p>
+  
+  <button
+    class="hello-btn-black-and-static w-56 mx-auto mt-12"
+  >
+    ō&nbsp;&nbsp;&nbsp;Continue with Hellō
+  </button>
+  
