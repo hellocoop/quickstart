a7198cb1fef6dcd9ae13219497d8d66e6ee98077
rohanharikr <rohanharikumar80@gmail.com>
2025-01-21 10:15:40 +0000

    fix

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
+import { mount } from 'svelte';
 import App from './App.svelte'
 import './index.css'
 
-const app = new App({
+const app = mount(App, {
   target: document.getElementById('app')
 })
-
-export default app
