546efb3e14d32b9fb258faab833cce9dc333c5ee
rohanharikr <rohanharikumar80@gmail.com>
2025-11-20 11:05:17 +0000

    Remove Threads link from footer in hello-dev-wc-footer.js for updated social media presence

--- public/assets/hello-dev-wc-footer.js
+++ public/assets/hello-dev-wc-footer.js
@@ -188,11 +188,6 @@ class Footer extends HTMLElement {
                 <a href="https://www.twitter.com/HelloCoop" target="_blank" aria-label="Go to Twitter page" rel="me">
                   <img src="https://cdn.hello.coop/images/twitter-icon.svg" alt="Twitter" height="16" width="16"/>
                   </a>
-                  </li>
-                  <li>
-                <a href="https://www.threads.net/@hello_coop" target="_blank" rel="me">
-                    <img src="https://cdn.hello.coop/images/threads-icon.svg" alt="Threads" height="16" width="16"/>
-                </a>
               </li>
               <li>
                 <a href="https://join.slack.com/t/hello-community/shared_invite/zt-1eccnd2np-qJoOWBkHGnpxvBpCTtaH9g" target="_blank" aria-label="Join Slack Community">
